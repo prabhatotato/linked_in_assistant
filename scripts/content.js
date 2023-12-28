d6da1268ac6eb9746171c743affa07a175eb8725
Prabhat Pundeer <prabhat12pundeer@gmail.com>
2023-12-28 11:12:28 +0530

    button for saved posts working

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+let ulHeader = document.querySelector(".global-nav__primary-items");
+
+let liViewPosts = document.createElement("li")
+liViewPosts.classList.add("global-nav__primary-item");
+
+let aViewPosts = document.createElement("a");
+
+aViewPosts.setAttribute("target", "_blank");
+aViewPosts.setAttribute("href", "https://www.linkedin.com/my-items/saved-posts/");
+aViewPosts.classList.add("app-aware-link", "global-nav__primary-link");
+//aViewPosts.innerHTML= "test";
+
+let spanViewPosts = document.createElement("span");
+spanViewPosts.classList.add("t-12", "break-words", "block", "t-black--light", "t-normalglobal-nav__primary-link-text");
+spanViewPosts.innerHTML = "Saved";
+
+aViewPosts.appendChild(spanViewPosts);
+liViewPosts.appendChild(aViewPosts);
+ulHeader.appendChild(liViewPosts);
