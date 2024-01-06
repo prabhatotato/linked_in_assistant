2ab19ed38601415bfc5790124bf7a37fd233d7d5
Prabhat Pundeer <prabhat12pundeer@gmail.com>
2024-01-06 19:12:07 +0530

    icon changed, saved-post button speech activated

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 let ulHeader = document.querySelector(".global-nav__primary-items");
 
-let liViewPosts = document.createElement("li")
+let liViewPosts = document.createElement("li");
 liViewPosts.classList.add("global-nav__primary-item");
 
 let aViewPosts = document.createElement("a");
@@ -8,7 +8,23 @@ let aViewPosts = document.createElement("a");
 aViewPosts.setAttribute("target", "_blank");
 aViewPosts.setAttribute("href", "https://www.linkedin.com/my-items/saved-posts/");
 aViewPosts.classList.add("app-aware-link", "global-nav__primary-link");
-//aViewPosts.innerHTML= "test";
+
+let divOuter = document.createElement("div");
+divOuter.classList.add("ivm-image-view-model", "global-nav__icon-ivm");
+
+let divInner = document.createElement("div");
+divInner.classList.add("ivm-view-attr__img-wrapper", "display-flex");
+
+let img = document.createElement("img");
+img.setAttribute("src", chrome.runtime.getURL("images/save.png"));
+img.setAttribute("id","imgSaved");
+
+divInner.appendChild(img);
+divOuter.appendChild(divInner);
+aViewPosts.appendChild(divOuter);
+
+
+
 
 let spanViewPosts = document.createElement("span");
 spanViewPosts.classList.add("t-12", "break-words", "block", "t-black--light", "t-normalglobal-nav__primary-link-text");
@@ -17,3 +33,26 @@ spanViewPosts.innerHTML = "Saved";
 aViewPosts.appendChild(spanViewPosts);
 liViewPosts.appendChild(aViewPosts);
 ulHeader.appendChild(liViewPosts);
+
+document.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(event) {
+    if (event.altKey && event.key === "s") { // Example: alt + a
+        aViewPosts.click();
+    }
+}
+
+// speech to text
+let speechRecog = new webkitSpeechRecognition();
+speechRecog.continuous = true;
+speechRecog.interimResults = true;
+speechRecog.lang = "en-in";
+speechRecog.start();
+
+speechRecog.onresult = function(event){
+    let transcript = event.results[event.resultIndex][0].transcript;
+    console.log(event);
+    if(transcript.trim().toLowerCase().includes("saved posts")){
+        aViewPosts.click();
+    }
+}
